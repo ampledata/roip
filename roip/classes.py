20b662998943903d49a14d36cd8d6634c7dcecb9
Greg Albrecht <gba@onbeep.com>
2018-01-17 17:46:34 -0800

    major updates

--- roip/classes.py
+++ roip/classes.py
@@ -3,6 +3,8 @@
 
 """Python Radio Over IP Module Class Definitions."""
 
+import logging
+
 from typing import List, Set, Dict, Tuple, Text, Optional, AnyStr
 
 import hidapi  # type: ignore
@@ -18,7 +20,17 @@ class RoIP(object):
 
     """Class for Radio Over IP interfaces."""
 
+    _logger = logging.getLogger(__name__)
+    if not _logger.handlers:
+        _logger.setLevel(roip.LOG_LEVEL)
+        _console_handler = logging.StreamHandler()
+        _console_handler.setLevel(roip.LOG_LEVEL)
+        _console_handler.setFormatter(roip.LOG_FORMAT)
+        _logger.addHandler(_console_handler)
+        _logger.propagate = False
+
     def __init__(self, device: tuple) -> None:
+        self._logger.debug('device="%s"', device)
         self.device: tuple = device
         self.hid_device = None
 
@@ -30,10 +42,11 @@ class RoIP(object):
         """Opens the connection to the RoIP device."""
         hidapi.hid_init()
         self.hid_device = hidapi.hid_open(self.device[0], self.device[1])
+        self._logger.info('Opened hid_device="%s"', self.hid_device)
 
     def close(self) -> None:
         """Closes the connection to the RoIP device."""
-        self.hid_device = hidapi.hid_close(self.device[0], self.device[1])
+        self.hid_device = hidapi.hid_close(self.hid_device)
 
     def ptt(self, callback=None) -> None:
         """
@@ -44,19 +57,34 @@ class RoIP(object):
         :param callback: Callback function to call (if specified).
         :type callback: def
         """
-        self.ptt_start()
+        self._logger.info('Transmitting...')
+        self._logger.debug('callback="%s"', callback)
+        wrote_start = self.ptt_start()
+        if not wrote_start:
+            raise 'Unable to Stop PTT. '
+
         callback()
-        self.ptt_stop()
+
+        wrote_stop = self.ptt_stop()
+        if not wrote_stop:
+            raise 'Unable to Stop PTT. '
 
     def ptt_start(self) -> int:
         """[TX] Triggers Transmit on the Radio."""
-        wrote = hidapi.hid_write(self.hid_device, roip.PTT_START)
+        start = roip.PTT_START
+        wrote = hidapi.hid_write(self.hid_device, start)
+        self._logger.debug('start="%s" wrote="%s"', start, wrote)
+        assert len(start) == wrote
         return wrote
 
     def ptt_stop(self) -> int:
         """[TX] Stops Transmit on the Radio."""
-        wrote = hidapi.hid_write(self.hid_device, roip.PTT_START)
+        stop = roip.PTT_STOP
+        wrote = hidapi.hid_write(self.hid_device, stop)
+        self._logger.debug('stop="%s" wrote="%s"', stop, wrote)
+        assert len(stop) == wrote
         return wrote
+
 
     def cor(self) -> bool:
         """
@@ -64,30 +92,37 @@ class RoIP(object):
 
         Blocks until a signal is received from the Radio.
         """
-        hid_read = hidapi.hid_read(self.hid_device, roip.READ_SIZE)
-
         cor_status: bool = False
+
+        hid_read = hidapi.hid_read(self.hid_device, roip.READ_SIZE)
+        self._logger.debug('read="%s"', hid_read)
+
         if roip.COR_START in hid_read:
             cor_status = True
         elif roip.COR_STOP in hid_read:
             cor_status = False
 
+        self._logger.debug('cor_status="%s"', cor_status)
         return cor_status
 
     # TODO: finish this:
-    def cor_nb(self, timeout=0.01) -> bool:
+    def cor_nb(self, timeout=100) -> bool:
         """
         [RX] Detects if there's a signal from the Radio.
 
         Non-blocking, returns after a timeout even if there's no signal from
         the Radio.
         """
-        hid_read = hidapi.hid_read(self.hid_device, roip.READ_SIZE)
-
         cor_status: bool = False
+
+        hid_read = hidapi.hid_read_timeout(
+            self.hid_device, roip.READ_SIZE, timeout)
+        self._logger.debug('read="%s"', hid_read)
+
         if roip.COR_START in hid_read:
             cor_status = True
         elif roip.COR_STOP in hid_read:
             cor_status = False
 
+        self._logger.debug('cor_status="%s"', cor_status)
         return cor_status
